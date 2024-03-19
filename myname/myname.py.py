04eff845cd0c46702b7a610ad7cda6b62ad9574a
Lukanik <deepakpaira5769@gmail.com>
2024-03-19 12:49:14 +0530

    first commit

---
+++ myname/myname.py.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+name = input("Enter Your Name: ")
+print(f"Your name is {name}...")
+
+names = ["Deepak", "deepak", "Babu", "babu", "Raja", "raja"]
+if name in names:
+    print("same same no different")
+else:
+    print("dono alag alag hai kya")
